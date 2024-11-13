bc4a9e935bc417b7ae437070259ed83eaee10676
darestar1 <karasuyasin06@gmail.com>
2024-11-13 10:35:38 +0300

    Created custom navigation bar and footer are done

---
+++ e-commerce/View/Home/LogoView.swift
@@ -0,0 +1,18 @@
+//
+//  LogoView.swift
+//  e-commerce
+//
+//  Created by Yasin Karasu on 13.11.2024.
+//
+
+import SwiftUI
+
+struct LogoView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LogoView()
+}
